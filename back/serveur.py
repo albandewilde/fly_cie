f832ed7b40ebd66ae69d4a66a7b2a5caf39b0d84
albandewilde <dewilde.alban@gmail.com>
2021-09-17 17:55:29 +0200

    Add price of flights

--- back/serveur.py
+++ back/serveur.py
@@ -7,13 +7,20 @@ import bottle
 srv = bottle.Bottle()
 
 flights = [
-    {"id": 1, "from": "DTW", "to": "CDG"},
-    {"id": 2, "from": "DTW", "to": "JFK"},
-    {"id": 3, "from": "CDG", "to": "JFK"},
-    {"id": 4, "from": "CDG", "to": "DTW"},
-    {"id": 5, "from": "JFK", "to": "CDG"},
-    {"id": 6, "from": "JFK", "to": "DTW"},
+    {"id": 1, "from": "DTW", "to": "CDG", "price": 700},
+    {"id": 2, "from": "DTW", "to": "JFK", "price": 300},
+    {"id": 3, "from": "CDG", "to": "JFK", "price": 1000},
+    {"id": 4, "from": "CDG", "to": "DTW", "price": 700},
+    {"id": 5, "from": "JFK", "to": "CDG", "price": 1000},
+    {"id": 6, "from": "JFK", "to": "DTW", "price": 300},
 ]
+
+
+def get_flight(flight_id):
+    for flight in flights:
+        if flight["id"] == flight_id:
+            return flight
+
 
 tickets = []
 
@@ -24,14 +31,19 @@ def root():
 
 
 @srv.post("/book")
-def create_ticket(lastName, firstName, nationality, flightId):
+def create_ticket():
+    body = json.loads(bottle.request.body.read().decode())
+
     ticket = {
-        "LastName": lastName,
-        "FirstName": firstName,
-        "Nationality": nationality,
-        "FlightId": flightId,
+        "last_name": body["last_name"],
+        "first_name": body["first_name"],
+        "nationality": body["nationality"],
+        "flight_id": int(body["flight_id"]),
+        "price": get_flight(int(body["flight_id"]))["price"],
     }
+
     tickets.append(ticket)
+
     return bottle.HTTPResponse(status=200, body=ticket)
 
 
