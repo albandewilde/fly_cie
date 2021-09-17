d6cf7b70597bee4dba9bf500e20e0f448988f9bc
Arthur <arthur.cheng@signature-code.com>
2021-09-17 17:44:44 +0200

    can book flight

--- back/serveur.py
+++ back/serveur.py
@@ -4,10 +4,21 @@ import bottle
 
 srv = bottle.Bottle()
 
+tickets = []
 
 @srv.get("/")
 def root():
     return "fly cie, how may I help you ?"
 
+@srv.post("/book")
+def create_ticket( lastName, firstName, nationality, flightId ):
+    ticket = {
+        "LastName": lastName,
+        "FirstName": firstName,
+        "Nationality": nationality,
+        "FlightId": flightId
+    }
+    tickets.append(ticket)
+    return bottle.HTTPResponse(status=200, body=ticket)
 
 srv.run(host="0.0.0.0", port="7860")
