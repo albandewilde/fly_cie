495cb565e1d1efd3640f4b7250f1dc6f0c2d6a18
Arthur <arthur.cheng@signature-code.com>
2021-09-20 11:56:28 +0200

    get round_trips

--- back/flights.py
+++ back/flights.py
@@ -72,21 +72,28 @@ def has_available_place(flight_id, flights):
                 return False
 
 
-# Global ticket
-# Global flight
 def book_ticket(
     user_last_name, user_first_name, nationality, flight_id, tickets, flights
 ):
-    ticket = {
-        "last_name": user_last_name,
-        "first_name": user_first_name,
-        "nationality": nationality,
-        "flight_id": int(flight_id),
-        "price": get_flight(int(flight_id), flights)["price"],
-        "creation_date": str(datetime.datetime.now()),
-    }
+    ticket = create_ticket()
     tickets.append(ticket)
 
     get_flight(flight_id, flights)["available_places"] -= 1
 
     return ticket
+
+def get_round_trips( flight_ids, flights ):
+    round_trips = []
+    trips = []
+
+    for id in flight_ids:
+        flight = get_flight( id, flights )   
+        for end_id in flight_ids[id:]:
+            second_flight = get_flight( id, flights )
+            if flight["from"] == second_flight["to"] and flight["to"] == second_flight["from"]:
+                round_trips.append( (flight, plane) )
+                break
+        else:
+            trips.append( flight )
+
+    return (round_trips, trips)
--- back/tickets.py
+++ back/tickets.py
@@ -1,1 +1,11 @@
 tickets = []
+
+def create_ticket(user_last_name, user_first_name, nationality, flight_id, price):
+    return {
+        "last_name": user_last_name,
+        "first_name": user_first_name,
+        "nationality": nationality,
+        "flight_id": int(flight_id),
+        "price": price,
+        "creation_date": str(datetime.datetime.now()),
+    }
